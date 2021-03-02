fdf6e52e25f861897988c998d4f9b25feed9d093
DustiasTheGuy <isakwebdev@gmail.com>
2021-03-02 01:31:47 +0100

    taking a bread

--- datacollector/datac.go
+++ datacollector/datac.go
@@ -1,11 +1,51 @@
 package datacollector
 
-type Datacollector struct {
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
+
+type DataCollector struct {
 }
 
-func New() *Datacollector {
+func New() *DataCollector {
 
-	dc := new(Datacollector)
+	dc := new(DataCollector)
 
 	return dc
+}
+
+// Collect should collect all information being requested
+func (d *DataCollector) Collect() error {
+	if err := d.FindAllLinks("https://en.wikipedia.org/wiki/Nilgai"); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// FindAllLinks grabs all links on a page and stores them in a slice
+func (d *DataCollector) FindAllLinks(url string) error {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	b := string(body)
+	regex := regexp.MustCompile(`<a\s+(?:[^>]*?\s+)?href="([^"]*)">`)
+	strs := regex.FindAllString(b, -1)
+
+	for i := 0; i < len(strs); i++ {
+		fmt.Println(strs[i])
+	}
+	return nil
 }
--- main/main.go
+++ main/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/DustiasTheGuy/datacollector/datacollector"
 )
 
 func main() {
 	dc := datacollector.New()
-
-	fmt.Println(dc)
+	dc.Collect()
 }
